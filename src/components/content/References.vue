778f4c1088c3b77f2b38a07e92eda06486577216
Luka Bacic <luka.bacic278@gmail.com>
2020-12-30 16:26:59 +1100

    Update URL regex so it must include the url protocol

--- src/components/content/References.vue
+++ src/components/content/References.vue
@@ -45,7 +45,7 @@ export default {
           'should be of type `string` in `References` component.';
         const linkErr = 'should be a valid URL in `References` component';
         const linkRegex = new RegExp(
-          /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/
+          /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#()?&//=]*)/gim
         );
 
         prop.forEach(item => {
